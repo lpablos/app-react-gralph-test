0b523ff7ab6bc3bddd5f37d4d9b3c29c66da8771
Luis Jorge <gevolutionxalapa@gmail.com>
2021-11-09 15:24:07 -0600

    Fix con eel esquema

--- server/src/graphql/resolvers/Query.js
+++ server/src/graphql/resolvers/Query.js
@@ -1,6 +1,13 @@
+import Message from "../../models/Message"
+
 const Query = {
     ping () {
         return "pong!"
+    },
+
+    // Funcion asincrona para obtener los resultados
+    messages: async () => {
+        return await Message.find()
     }
 }
 
